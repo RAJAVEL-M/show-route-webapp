79c39f412219f0007249aba1294c4a4dd94a9bf2
Rajavel M <rvelusma@gmail.com>
2021-10-06 17:44:58 +0530

    first commit

---
+++ route/views.py
@@ -0,0 +1,22 @@
+import folium
+from django.shortcuts import render,redirect
+from . import getroute
+
+
+def showmap(request):
+    return render(request,'showmap.html')
+
+def showroute(request,lat1,long1,lat2,long2):
+    figure = folium.Figure()
+    lat1,long1,lat2,long2=float(lat1),float(long1),float(lat2),float(long2)
+    route=getroute.get_route(long1,lat1,long2,lat2)
+    m = folium.Map(location=[(route['start_point'][0]),
+                                 (route['start_point'][1])], 
+                       zoom_start=10)
+    m.add_to(figure)
+    folium.PolyLine(route['route'],weight=8,color='blue',opacity=0.6).add_to(m)
+    folium.Marker(location=route['start_point'],icon=folium.Icon(icon='play', color='green')).add_to(m)
+    folium.Marker(location=route['end_point'],icon=folium.Icon(icon='stop', color='red')).add_to(m)
+    figure.render()
+    context={'map':figure}
+    return render(request,'showroute.html',context)
